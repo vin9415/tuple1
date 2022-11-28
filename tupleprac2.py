a749b172888c7efbe2177b327740e40631082ae5
vin9415 <114800197+vin9415@users.noreply.github.com>
2022-11-28 21:43:06 +0530

    Add files via upload

---
+++ tupleprac2.py
@@ -0,0 +1,11 @@
+tuple1=(10,20)
+tuple2=(30,40)
+# #swap
+# temp = tuple1
+# tuple1 = tuple2
+# tuple2= temp
+# print(tuple1)
+# print(tuple2)
+(tuple1,tuple2)=(tuple2,tuple1)
+print(tuple1)
+print(tuple2)
